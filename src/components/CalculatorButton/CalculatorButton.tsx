578c4494138077ba5e4efe15914b4be58f589da5
Солтангазиева Мария Алексеевна <avemaryplus@gmail.com>
2023-07-24 19:09:48 +0600

    #1 Создан компонент для отображения кнопки

---
+++ src/components/CalculatorButton/CalculatorButton.tsx
@@ -0,0 +1,37 @@
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  GestureResponderEvent,
+} from "react-native";
+
+type Props = {
+  label: string;
+  onPress: (label: string) => void;
+};
+
+const CalculatorButton = ({ label, onPress }: Props) => {
+  return (
+    <TouchableOpacity onPress={() => onPress(label)} style={styles.button}>
+      <Text style={styles.label}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 80,
+    height: 80,
+    margin: 5,
+    borderRadius: 20,
+    backgroundColor: "#021091",
+  },
+  label: {
+    fontSize: 30,
+    color: "#fff",
+  },
+});
+
+export default CalculatorButton;
